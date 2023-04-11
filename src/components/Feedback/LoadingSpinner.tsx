6dd81f0f0fa8955a99b168ef5fbecd07f896eaa3
claudiodantas <claudiodantas1996@gmail.com>
2023-04-11 09:30:48 -0300

    create new loading spinner

--- src/components/Feedback/LoadingSpinner.tsx
+++ src/components/Feedback/LoadingSpinner.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-
 import styles from "./style.module.css";
 
 const LoadingSpinner = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.spinner}></div>
+    <div className={styles.lds_ripple}>
+      <div></div>
+      <div></div>
     </div>
   );
 };
