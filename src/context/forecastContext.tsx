e80158824c7f57d37649229927559fe123fe1600
claudiodantas <claudiodantas1996@gmail.com>
2023-11-29 15:00:45 -0300

    add error handling

--- src/context/forecastContext.tsx
+++ src/context/forecastContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from "react";
 
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 
 import { WeatherData } from "../types";
 
@@ -8,18 +8,29 @@ export type ForecastContextType = {
   state: WeatherData;
   handleSearch: (city: string) => void;
   isLoading: boolean;
+  error: Error | undefined;
 };
 
 const ForecastContext = createContext<ForecastContextType>(
   {} as ForecastContextType
 );
 
+type Error = {
+  error: {
+    code: number;
+    message: string;
+  };
+};
+
 const ForecastProvider = ({ children }: any) => {
   const [state, setState] = useState<WeatherData>({} as WeatherData);
+  const [error, setError] = useState<Error>(null as unknown as Error);
+
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async (city: string | undefined) => {
     setIsLoading(true);
+    setError(null as unknown as Error);
     try {
       const response = await axios.get(
         `https://api.weatherapi.com/v1/forecast.json?key=f4b344c2a21f428e90011107232711&q=${city}&days=3&aqi=no&alerts=no
@@ -31,17 +42,18 @@ const ForecastProvider = ({ children }: any) => {
         }
       );
       const data = await response.data;
-      console.log(data);
       setState(data);
     } catch (error) {
-      console.log(error);
+      if (isAxiosError(error)) {
+        setError(error.response?.data);
+      }
     } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <ForecastContext.Provider value={{ state, handleSearch, isLoading }}>
+    <ForecastContext.Provider value={{ state, handleSearch, isLoading, error }}>
       {children}
     </ForecastContext.Provider>
   );
