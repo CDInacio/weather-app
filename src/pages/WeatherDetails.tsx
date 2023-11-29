e93134b566206728c718e242e1277c1b62ad6c55
claudiodantas <claudiodantas1996@gmail.com>
2023-11-29 20:29:12 -0300

    remove card component

--- src/pages/WeatherDetails.tsx
+++ src/pages/WeatherDetails.tsx
@@ -10,7 +10,6 @@ import { useWeather } from "../context/forecastContext";
 const WeatherDetails = () => {
   const { city } = useParams<{ city: string }>();
   const { handleSearch, isLoading, error } = useWeather();
-
   let content = null;
 
   useEffect(() => {
@@ -19,8 +18,8 @@ const WeatherDetails = () => {
 
   if (error?.error.code && !isLoading) {
     content = (
-      <Card className="flex flex-col items-center">
-        <SearchInput className="my-5 w-full" />
+      <div className="w-screen h-screen justify-center flex flex-col items-center">
+        <SearchInput className="my-5 w-[300px] sm:w-[400px] md:w-[450px]" />
         <p className="font-medium text-sm">
           Não foi possível encontrar informações sobre o local.
         </p>
@@ -29,11 +28,11 @@ const WeatherDetails = () => {
           src="/images/no-data.svg"
           className="w-[300px] h-[300px]"
         />
-      </Card>
+      </div>
     );
   }
 
-  if (isLoading || !isLoading) {
+  if (isLoading) {
     content = (
       <div className="w-screen h-screen overflow-hidden flex flex-col items-center justify-center ">
         <div className="flex justify-center items-center ">
@@ -60,11 +59,7 @@ const WeatherDetails = () => {
     );
   }
 
-  return (
-    <div className="px-5 overflow-x-hidden">
-      {content}
-    </div>
-  );
+  return <div className="px-5 overflow-x-hidden">{content}</div>;
 };
 
 export default WeatherDetails;
