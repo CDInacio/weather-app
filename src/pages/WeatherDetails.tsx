51b99dd9f84820cdbd787685fc8ec34ab4d5ea3c
claudiodantas <claudiodantas1996@gmail.com>
2023-11-29 15:01:44 -0300

    render error component conditionally

--- src/pages/WeatherDetails.tsx
+++ src/pages/WeatherDetails.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import Card from "../components/Card/Card";
 import CurrentIndicators from "../components/CurrentIndicators/CurrentIndicators";
 import Forecast from "../components/Forecast/Forecast";
 import SearchInput from "../components/input/searchInput";
@@ -8,13 +9,29 @@ import { useWeather } from "../context/forecastContext";
 
 const WeatherDetails = () => {
   const { city } = useParams<{ city: string }>();
-  const { handleSearch, isLoading } = useWeather();
+  const { handleSearch, isLoading, error } = useWeather();
 
   let content = null;
 
   useEffect(() => {
     handleSearch(city!);
   }, [city]);
+
+  if (error?.error.code && !isLoading) {
+    content = (
+      <Card className="flex flex-col items-center">
+        <SearchInput className="my-5" />
+        <p className="font-medium text-sm">
+          Não foi possível encontrar informações sobre o local.
+        </p>
+        <img
+          alt="icone de informação não encontrada"
+          src="/images/no-data.svg"
+          className="w-[300px] h-[300px]"
+        />
+      </Card>
+    );
+  }
 
   if (isLoading) {
     content = (
@@ -27,10 +44,10 @@ const WeatherDetails = () => {
     );
   }
 
-  if (!isLoading) {
+  if (!isLoading && !error?.error.code) {
     content = (
       <>
-        <div className=" rounded  p-10 max-w-[1200px] absolute  bg-white">
+        <Card>
           <SearchInput className="my-5" />
           <h1 className="text-2xl font-medium mb-6">Resumo de hoje</h1>
           <CurrentIndicators />
@@ -38,7 +55,7 @@ const WeatherDetails = () => {
             <h1 className="text-2xl font-medium my-6">Próximos 3 dias</h1>
             <Forecast />
           </div>
-        </div>
+        </Card>
       </>
     );
   }
