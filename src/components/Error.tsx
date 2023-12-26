d827ea6c2a6825326f2b5da3c4b1349d54ba8e5c
claudiodantas <claudiodantas1996@gmail.com>
2023-12-26 16:50:28 -0300

    feat: add a fallback error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+const Error = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="w-screen h-screen text-sm md:text-base px-5 flex items-center justify-center flex-col">
+      <img src="/images/error.svg" className="w-[400px] h-[400px] " />
+      <p className="font-semibold">
+        Oops, um erro inesperado aconteceu! Tente novamente mais tarde.
+      </p>
+      <button className="mt-2 " onClick={handleNavigate}>
+        Início
+      </button>
+    </div>
+  );
+};
+
+export default Error;
